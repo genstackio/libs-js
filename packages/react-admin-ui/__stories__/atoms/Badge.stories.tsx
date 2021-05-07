cbd2559e629016fab3af8b06ebbbedd0dbbc7890
koala819 <x.genolhac@gmail.com>
2021-05-07 14:12:38 +0200

    feat: CU-jd52c4 create Badge

--- packages/react-admin-ui/__stories__/atoms/Badge.stories.tsx
+++ packages/react-admin-ui/__stories__/atoms/Badge.stories.tsx
@@ -6,14 +6,21 @@ export default {
     title: 'Atoms/Badge',
     component: Badge,
     argTypes: {
-        children: { control: 'text' },
-        color: { control: {type: 'select'}, options: ['primary', 'secondary', 'default'] },
-        variant: { control: {type: 'select'}, options: ['filled', 'outlined', 'contained'] },
-        disabled: { control: 'boolean' }
     },
 
 }
 
-const Template = args => <Badge {...args} />;
+const Template = args => (
+    <div className={'flex flex row items-center space-x-6'}>
+        <Badge {...args} color={'default'} text={'1'} />
+        <Badge {...args} color={'secondary'} text={'2'} />
+        <Badge {...args} color={'success'} text={'3'} />
+        <Badge {...args} color={'info'} text={'4'} />
+        <Badge {...args} color={'warning'} text={'5'} />
+        <Badge {...args} color={'danger'} text={'6'} />
+        <Badge {...args} color={'light'} text={'7'} />
+        <Badge {...args} color={'dark'} text={'8'} />
+    </div>
+);
 
 export const basic = s(Template, {variant: "pill"})
