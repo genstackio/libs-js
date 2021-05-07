25809265a6877d52a23d8a79b60c50a0d43c3d81
carl <carl.mrchnd@gmail.com>
2021-05-07 12:14:39 +0200

    feat: CU-jdbfdm create button

--- packages/react-admin-ui/src/atoms/Button.tsx
+++ packages/react-admin-ui/src/atoms/Button.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
+import clsx from "clsx";
+import colorClass from "../utils/colorClass";
 
-export function Button() {
+export function Button({children, color = 'primary', variant = 'filled', large, disabled}: ButtonProps) {
     return (
-        <button>Hello</button>
+        <button disabled={disabled} className={clsx(colorClass({color, variant, disabled}), 'py-2 px-4 rounded', disabled && 'opacity-50 cursor-not-allowed', large && 'w-60')}>{children || ''}</button>
     );
 }
 
 export interface ButtonProps {
+    children?: ReactNode,
+    color?: 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark',
+    variant?: 'filled' | 'outlined' | 'contained',
+    large?: boolean,
+    disabled?: boolean,
 }
 
 export default Button
