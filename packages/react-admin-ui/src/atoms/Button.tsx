8b6985307812c82a65704bfc2edf1c95c259207e
carl <carl.mrchnd@gmail.com>
2021-05-10 15:24:40 +0200

    feat: CU-jh76ve create modal

--- packages/react-admin-ui/src/atoms/Button.tsx
+++ packages/react-admin-ui/src/atoms/Button.tsx
@@ -3,9 +3,9 @@ import clsx from "clsx";
 import colorClass from "../utils/colorClass";
 import {box_color, box_variant} from '../types';
 
-export function Button({children, color = 'primary', variant = 'filled', large, disabled}: ButtonProps) {
+export function Button({children, color = 'primary', variant = 'filled', large, disabled, onClick}: ButtonProps) {
     return (
-        <button disabled={disabled} className={clsx(colorClass({color, variant, disabled}), 'py-2 px-4 rounded', disabled && 'opacity-50 cursor-not-allowed', large && 'w-60')}>{children || ''}</button>
+        <button disabled={disabled} className={clsx(colorClass({color, variant, disabled}), 'py-2 px-4 rounded', disabled && 'opacity-50 cursor-not-allowed', large && 'w-60')} onClick={onClick}>{children || ''}</button>
     );
 }
 
@@ -15,6 +15,7 @@ export interface ButtonProps {
     variant?: box_variant,
     large?: boolean,
     disabled?: boolean,
+    onClick?: any,
 }
 
 export default Button
--- packages/react-admin-ui/src/molecules/Modal.tsx
+++ packages/react-admin-ui/src/molecules/Modal.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
+import {Button} from "../atoms";
+import MuiModal from "@material-ui/core/Modal";
 
-export function Modal({}: ModalProps) {
+export function Modal({children, title, buttonsItems, opened = false, onClose}: ModalProps) {
     return (
-        <div>MODAL</div>
+        <MuiModal className={'flex justify-center items-center'} open={opened}>
+            <div className={'bg-white border border-gray-300 rounded-md w-8/12 sm:w-11/12 xs:w-full'}>
+                <div className={'flex flex-row-reverse justify-between items-center m-4'}>
+                    <Button color={'dark'} onClick={onClose}>X</Button>
+                    {title && <div className={'text-xl font-bold'}>{title}</div>}
+                </div>
+                <hr />
+                {children || ''}
+                {buttonsItems && <div>
+                    <hr />
+                    <div className={'flex justify-end space-x-4 m-4'}>
+                        {buttonsItems.map(({ label, variant, color, onClick }) => (
+                            <Button variant={variant} color={color} onClick={onClick}>{label}</Button>
+                        ))}
+                    </div>
+                </div>}
+            </div>
+        </MuiModal>
     );
 }
 
 export interface ModalProps {
+    title?: string,
+    buttonsItems?: any,
+    opened?: boolean,
+    onClose?: Function,
+    children?: ReactNode,
 }
 
 export default Modal
