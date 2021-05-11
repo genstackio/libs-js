be0bb01d2ec0982edc435d8655769636af99a393
carl-mrchnd <66088573+carl-mrchnd@users.noreply.github.com>
2021-05-11 10:46:37 +0200

    feat: CU-jh76ve create modal (#11)

---
+++ packages/react-admin-ui/__stories__/molecules/Modal.stories.tsx
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {s} from "../utils";
+import {Button, Modal} from '../../src';
+
+export default {
+    title: 'Molecules/Modal',
+    component: Modal,
+    argTypes: {
+        title: { control: {type: 'text'} },
+        buttonsItems: { control: {type: 'object'} },
+        opened: { control: {type: 'boolean'} },
+        onClose: { control: {type: 'object'} },
+    },
+}
+
+const Template = args => {
+    const [opened, setOpened] = useState(false);
+    const handleOpen = () => {
+        setOpened(true);
+    };
+    const handleClose = () => {
+        setOpened(false);
+    };
+    const title = 'New message';
+    const content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    const buttonsItems = [
+        {
+            variant: 'contained',
+            color: 'secondary',
+            onClick: handleClose,
+            label: 'Close',
+        },
+        {
+            variant: 'contained',
+            color: 'primary',
+            onClick: () => alert('Clicked'),
+            label: 'Send message',
+        },
+    ]
+
+    return (
+        <div>
+            <Button onClick={handleOpen}>Open</Button>
+            <Modal title={title} opened={opened} onClose={handleClose} buttonsItems={buttonsItems}>
+                <div className={'text-sm m-4'}>{content}</div>
+            </Modal>
+        </div>
+    )
+};
+
+export const basic = s(Template, {});
