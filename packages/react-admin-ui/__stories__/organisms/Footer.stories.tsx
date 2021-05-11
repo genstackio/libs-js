98199675da2ab17a16a9ae0084d2328ece913ade
carl-mrchnd <66088573+carl-mrchnd@users.noreply.github.com>
2021-05-11 12:45:51 +0200

    feat: CU-jk7k5c create footer (#13)

---
+++ packages/react-admin-ui/__stories__/organisms/Footer.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {s} from "../utils";
+import {Footer} from '../../src';
+
+export default {
+    title: 'Organisms/Footer',
+    component: Footer,
+    argTypes: {
+        content: { control: {type: 'text'} },
+    },
+}
+
+const Template = args => <Footer {...args} />
+
+export const basic = s(Template, {
+    content: 'Footer Text Example'
+});
