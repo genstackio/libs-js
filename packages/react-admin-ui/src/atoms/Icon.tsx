a9cd270d2e54b5aa1609d310fe794dabec2995ad
carl <carl.mrchnd@gmail.com>
2021-05-12 16:23:19 +0200

    style: CU-jq9axh remove border accordion

---
+++ packages/react-admin-ui/src/atoms/Icon.tsx
@@ -0,0 +1,16 @@
+import React, {ReactNode} from "react";
+
+export function Icon({icon, ...props}) {
+    switch (typeof icon) {
+        case 'string':
+            return <img src={icon} {...props} />;
+        default:
+            return icon;
+    }
+}
+
+export interface IconProps {
+    icon?: string | ReactNode,
+}
+
+export default Icon
--- packages/react-admin-ui/src/molecules/Accordion.tsx
+++ packages/react-admin-ui/src/molecules/Accordion.tsx
@@ -2,27 +2,38 @@ import React from 'react';
 import clsx from "clsx";
 import colorClass from "../utils/colorClass";
 import {box_color, box_variant} from "../types";
+import {makeStyles} from "@material-ui/core/styles";
 import MuiAccordion from "@material-ui/core/Accordion";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
 import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+import {Icon} from "../atoms";
+
+const useStyles = makeStyles({
+    root: {
+        '&.MuiAccordion-root:before': {
+            backgroundColor: 'transparent'
+        }
+    }
+});
 
 export function Accordion({items = [], color, variant = 'contained'}: AccordionProps) {
+    const classes = useStyles();
     return (
         <div>
             {items.map(({icon, title, content}, index) => (
-                <MuiAccordion key={index} className={'rounded-b-md mb-2'}>
+                <MuiAccordion elevation={0} key={index} classes={classes} className={clsx(classes.root, 'rounded-t-md mb-2')} square={true}>
                     <MuiAccordionSummary
                         expandIcon={<ExpandMoreIcon className={clsx(colorClass({color, variant}))} />}
                         aria-controls={`panel${index}-content`} id={`panel${index}-header`}
                         className={clsx('rounded-t-md', colorClass({color, variant}))}
                     >
                         <div className={'flex flex-row items-center space-x-2'}>
-                            {icon && <img src={icon} alt={title} />}
+                            {icon && <Icon icon={icon} />}
                             <div className={'font-bold'}>{title}</div>
                         </div>
                     </MuiAccordionSummary>
-                    <MuiAccordionDetails className={'py-3 px-4 shadow-container'}>
+                    <MuiAccordionDetails className={'py-3 px-4'}>
                         <div className={'text-sm tracking-wider'}>{content}</div>
                     </MuiAccordionDetails>
                 </MuiAccordion>
