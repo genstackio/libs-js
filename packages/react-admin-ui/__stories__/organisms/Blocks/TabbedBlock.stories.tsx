1f0977189a50d2e0f9a551debcd2ea9d440253d5
carl <carl.mrchnd@gmail.com>
2021-05-14 09:20:14 +0200

    feat: CU-jk7nzh create tabbed block

---
+++ packages/react-admin-ui/__stories__/organisms/Blocks/TabbedBlock.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {s} from "../../utils";
+import {TabbedBlock} from '../../../src';
+
+export default {
+    title: 'Organisms/Blocks/TabbedBlock',
+    component: TabbedBlock,
+    argTypes: {
+        items: { control: {type: 'object'} },
+        color: { control: {type: 'select'}, options: ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark'] },
+        variant: { control: {type: 'select'}, options: ['filled', 'outlined', 'contained'] },
+    },
+}
+
+const Template = args => <TabbedBlock {...args} />;
+
+export const basic = s(Template, {
+    items: [
+        {
+            title: 'Home',
+            content: 'Home Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
+        },
+        {
+            title: 'Profile',
+            content: 'Profile Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
+        },
+        {
+            title: 'Disabled',
+            content: 'Disabled Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+            disabled: true,
+        },
+        {
+            title: 'Contact',
+            content: 'Contact Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
+        },
+    ]
+});
---
+++ packages/react-admin-ui/src/organisms/blocks/TabbedBlock.tsx
@@ -0,0 +1,58 @@
+import React, {useCallback} from 'react';
+import {Block} from "../../atoms";
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import AppBar from '@material-ui/core/AppBar';
+import colorClass from "../../utils/colorClass";
+import {box_color, box_variant} from "../../types";
+
+function TabPanel({children, value, index, ...props}: TabPanelProps) {
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...props}
+        >
+            {value === index && (
+                <div className={'p-3 text-sm'}>{children}</div>
+            )}
+        </div>
+    );
+}
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: any;
+    value: any;
+}
+
+export function TabbedBlock({items = [], color, variant = 'outlined'}: TabbedBlockProps) {
+    const [value, setValue] = React.useState(0);
+    const handleChange = useCallback((event: React.ChangeEvent<{}>, newValue: number) => setValue(newValue), [setValue]);
+    return (
+        <Block variant={variant} color={color}>
+            <AppBar position="static" elevation={0} color={'transparent'}>
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" className={'bg-transparent border-b-1 border-gray-200'} TabIndicatorProps={{className: colorClass({color, variant})}}>
+                    {items.map(({title, disabled}, index) => (
+                        <Tab label={title} key={index} id={`simple-tab-${index}`} aria-controls={`simple-tabpanel-${index}`} disabled={disabled} className={'focus:outline-none'} />
+                    ))}
+                </Tabs>
+            </AppBar>
+            {items.map(({content}, index) => (
+                <TabPanel value={value} index={index} key={index}>
+                    {content}
+                </TabPanel>
+            ))}
+        </Block>
+    );
+}
+
+export interface TabbedBlockProps {
+    items?: {title?: any, content?: any, disabled?: boolean}[],
+    color?: box_color,
+    variant?: box_variant,
+}
+
+export default TabbedBlock
