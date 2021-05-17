30df605dcb0876d59b74e14d44fd93d43f4db788
Xavier GENOLHAC <70317658+koala819@users.noreply.github.com>
2021-05-17 18:29:00 +0200

    feat: CU-jhb9ru (#27)

---
+++ packages/react-admin-ui/__stories__/organisms/Blocks/FiguresBlock.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {s} from "../../utils";
+import {FiguresBlock} from '../../../src';
+
+export default {
+    title: 'Organisms/Blocks/FiguresBlock',
+    component: FiguresBlock,
+    argTypes: {
+        items: { control: {type: 'object'} },
+        color: { control: {type: 'select'}, options: ['primary', 'secondary', 'success', 'info', 'warning', 'danger', 'light', 'dark'] },
+        variant: { control: {type: 'select'}, options: ['filled', 'outlined', 'contained'] },
+    },
+}
+
+const Template = args => <FiguresBlock {...args} />;
+
+export const basic = s(Template, {
+    title: 'Our Sale Value',
+    price: '$745425',
+    tag: {text: 'New', color: 'success'},
+    pill: {
+        text: 'coeur',
+        color: 'warning'}
+});
