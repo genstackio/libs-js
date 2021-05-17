d0192b58f2953b79da0f4b604007be3ce6520314
koala819 <x.genolhac@gmail.com>
2021-05-17 14:46:45 +0200

    feat: CU-jhb9ru

--- packages/react-admin-ui/src/organisms/blocks/FiguresBlock.tsx
+++ packages/react-admin-ui/src/organisms/blocks/FiguresBlock.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import {Alert, Tag, Pill} from "../../atoms";
 
-export function FiguresBlock({}: FiguresBlockProps) {
+export function FiguresBlock({ title, price, pill, tag, ...props }: FiguresBlockProps) {
     return (
-        <div>FIGURES BLOCK</div>
+        <Alert {...props}>
+            <div className={'flex '}>
+            <div>
+                {title && <div className= {'text-2xl mr-8 inline-block'}>{title}</div>}
+                <div className= {'inline-block'}>
+                    <Tag text= { tag.text} color= {tag.color} />
+                </div>
+                <div className={'text-black font-black text-4xl mt-4'}>
+                    {price}
+                </div>
+            </div>
+            <div className={'absolute right-4'}>
+                <Pill text= {pill.text} color= {pill.color} />
+            </div>
+            </div>
+        </Alert>
     );
 }
 
 export interface FiguresBlockProps {
+    title?: string,
+    price?: string,
+    pill?: any,
+    tag?: any,
 }
 
 export default FiguresBlock
