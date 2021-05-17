eddec68e4c392f454a3fa8873610497c9500691f
carl <carl.mrchnd@gmail.com>
2021-05-17 14:36:48 +0200

    feat: CU-jhba2a create heading left block

--- packages/react-admin-ui/src/organisms/blocks/HeadingLeftBlock.tsx
+++ packages/react-admin-ui/src/organisms/blocks/HeadingLeftBlock.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import Text from "../../atoms/Text";
+import Block from "../../atoms/Block";
+import {box_color, box_variant} from "../../types";
 
-export function HeadingLeftBlock({}: HeadingLeftBlockProps) {
+export function HeadingLeftBlock({color, variant = 'filled', title, subtitle, description, btnLabel}: HeadingLeftBlockProps) {
     return (
-        <div>HEADING LEFT BLOCK</div>
+        <Block variant={variant} color={color} title={title} btnLabel={btnLabel}>
+            <div className={'space-y-2'}>
+                <Text text={subtitle} variant={'body'} />
+                <Text text={description} variant={'description'} />
+            </div>
+        </Block>
     );
 }
 
 export interface HeadingLeftBlockProps {
+    title?: string,
+    subtitle?: string,
+    description?: string,
+    btnLabel?: string,
+    color?: box_color,
+    variant?: box_variant,
 }
 
 export default HeadingLeftBlock
