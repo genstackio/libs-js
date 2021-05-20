86a105b9d06b6dde661b61123d4ee43de537ff0b
AugustinBoju <bojuaugustin@gmail.com>
2021-05-20 18:02:34 +0200

    feat: CU-k57q2k_composant_molecules-BulletPoint

---
+++ packages/react-admin-ui/src/molecules/BulletPoint.tsx
@@ -0,0 +1,30 @@
+import {ReactNode} from 'react';
+import clsx from "clsx";
+import Icon from "../atoms/Icon";
+import {BaseBlockProps, Block} from "../atoms";
+
+export function BulletPoint({title, text, icon, ...props}: BulletPointProps) {
+    return (
+        <Block {...props}>
+            <div className={clsx('rounded-2xl shadow-block')}>
+                <div className='p-5 flex items-center'>
+                    {icon && <Icon icon={icon} />}
+                    <div className='flex-column p-3'>
+                        <h6 className={'mt-4 text-2xl font-bold'}>{title}</h6>
+                        <p className=''>{text || ''}</p>
+                    </div>
+                </div>
+            </div>
+        </Block>
+    );
+}
+
+export interface BulletPointProps extends BaseBlockProps {
+    icon?: string | ReactNode,
+    variant?: any,
+    color?: any,
+    title?: ReactNode,
+    text?: ReactNode,
+}
+
+export default BulletPoint
