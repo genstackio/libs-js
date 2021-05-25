4c11d3b53179cbe09e57227cb14744f863896c6c
koala819 <x.genolhac@gmail.com>
2021-05-25 15:13:16 +0200

    feat: CU-k74t8m create ItemCard

--- packages/react-admin-ui/src/molecules/ItemCard.tsx
+++ packages/react-admin-ui/src/molecules/ItemCard.tsx
@@ -1,23 +1,23 @@
-import {Block, Icon} from "../atoms";
+import {Block, Tag, Icon} from "../atoms";
+import Text from "../atoms/Text";
 import {box_color, box_variant} from "../types";
 
-export function ItemCard({title, subTitle, text, size, icon, ...props}: ItemCardProps) {
+export function ItemCard({title, subTitle, description, date, colorTitle , colorSubTitle, colorDate, colorDescription, blockVariant='outlined', blockColor ='light', icon, size}: ItemCardProps) {
     return (
-        <Block variant={props.blockVariant} color={props.blockColor}>
-            <div className='absolute left-4'>
-                    <div className="grid grid-rows-2 grid-flow-col gap-4">
-                        <div className='row-span-2 ml-4'>
-                            {icon ? <Icon size={size}  icon={icon} /> : <div/>}
-                        </div>
-                        <div className='row-span-1'>
-                            <h1 className='font-extrabold text-3xl'>{title}</h1>
-                        </div>
-                        <div className='flex inline-block row-span-1 -mt-4'>
-                            <h2 className='text-gray-400 font-bold'>{subTitle}</h2>
-                        </div>
+        <Block variant={blockVariant} color={blockColor} >
+            <div className="flex justify-between mb-6">
+                <div className="flex mb-6">
+                    <div className='row-span-2 ml-4 mt-2'>
+                        {icon ? <Icon size={size}  icon={icon} /> : <div/>}
+                    </div>
+                    <div>
+                        <Text text={title} color={colorTitle} variant={'title5'} />
+                        <Text text={subTitle} color={colorSubTitle} variant={'subtitle'} />
                     </div>
                 </div>
-            <p className='pt-20'>{text}</p>
+                <div className='mt-2'>{date ? <Text text={date} color={colorDate} variant={'overline'} /> : <Tag text="new"/>}</div>
+            </div>
+            <Text text={description} color={colorDescription} variant={'body'} />
         </Block>
     );
 }
@@ -25,9 +25,14 @@ export function ItemCard({title, subTitle, text, size, icon, ...props}: ItemCard
 export interface ItemCardProps {
     title?: string,
     subTitle?: string,
-    text?: string,
+    description?: string,
     blockVariant?: box_variant,
     blockColor?: box_color,
+    date?: string,
+    colorDate?: box_color,
+    colorTitle?: box_color,
+    colorSubTitle?: box_color,
+    colorDescription?: box_color,
     icon?: string,
     size?: number,
 }
