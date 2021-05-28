a17dacc7447e93c19a1ed687b5b4ae107f6c700a
AugustinBoju <bojuaugustin@gmail.com>
2021-05-28 09:31:51 +0200

    feat: CU-kh8u6z_Composant_molecules--Menu

---
+++ packages/react-admin-ui/__stories__/molecules/Menu.stories.tsx
@@ -0,0 +1,103 @@
+import {Menu} from '../../src'
+import {args, s} from "../utils";
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import AirplayOutlinedIcon from '@material-ui/icons/AirplayOutlined';
+
+export default {
+    title: 'Molecules/Menu',
+    component: Menu,
+    argTypes: {
+        icon: args.icon,
+        title: args.title,
+        text: args.children,
+        color: args.color,
+        variant: args.blockVariant,
+        padding: args.padding,
+        items: args.items,
+    },
+}
+
+const Template = args => <Menu {...args} />;
+
+export const basic = s(Template,  {
+    title: "General",
+    text: "Dashboard, Widgets & Layout.",
+    items: [
+        {
+            icon: <HomeOutlinedIcon/>,
+            title: 'Dashboard',
+            badge: {
+                type: 'tag',
+                label: '2',
+                color: 'success',
+            },
+            content: [{
+                    label: 'Default',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                },
+                {
+                    label: 'Ecommerce',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                }
+            ]
+        },
+        {
+            icon: <AirplayOutlinedIcon/>,
+            title: 'Widgets',
+            badge: {
+                type: 'pill',
+                label: '2',
+                color: 'danger',
+            },
+            content: [{
+                label: 'General',
+                target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+            },
+                {
+                    label: 'Chart',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                }
+            ]
+        },
+        {
+            icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                       stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                       className="feather feather-layout">
+                        <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                        <line x1="3" y1="9" x2="21" y2="9"></line>
+                        <line x1="9" y1="21" x2="9" y2="9"></line>
+                    </svg>,
+            title: 'Page Layout',
+            content: [
+                {
+                    label: 'Boxed',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                },
+                {
+                    label: 'RTL',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                },
+                {
+                    label: 'Dark Layout',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                },
+                {
+                    label: 'Hide Navbar Scroll',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                },
+                {
+                    label: 'Footer Light',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                },
+                {
+                    label: 'Footer Dark',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                },
+                {
+                    label: 'Footer Fixed',
+                    target: 'https://www.google.com/webhp?hl=fr&sa=X&ved=0ahUKEwj8oNu2_uvwAhUiBmMBHeFQB30QPAgI',
+                },
+            ]
+        }
+    ]
+})
--- packages/react-admin-ui/src/molecules/Accordion.tsx
+++ packages/react-admin-ui/src/molecules/Accordion.tsx
@@ -1,12 +1,13 @@
 import clsx from "clsx";
 import colorClass from "../utils/colorClass";
-import {box_color, box_variant} from "../types";
+import {badge_variant, box_color, box_variant} from "../types";
 import {makeStyles} from "@material-ui/core/styles";
 import MuiAccordion from "@material-ui/core/Accordion";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
 import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
-import {Icon} from "../atoms";
+import {Icon, Badge} from "../atoms";
+import {ChangeEvent, useState, useCallback} from "react";
 
 const useStyles = makeStyles({
     root: {
@@ -16,24 +17,36 @@ const useStyles = makeStyles({
     }
 });
 
-export function Accordion({items = [], color, variant = 'contained'}: AccordionProps) {
+export function Accordion({items = [], color, variant}: AccordionProps) {
     const classes = useStyles();
+    const [expanded, setExpanded] = useState<number | false>(false);
+    const handleChange = useCallback((panel: number) => (event: ChangeEvent<{}>, newExpanded: boolean) => {
+        setExpanded(newExpanded ? panel : false)}, [setExpanded]);
     return (
         <div>
-            {items.map(({icon, title, content}, index) => (
-                <MuiAccordion elevation={0} key={index} classes={classes} className={clsx(classes.root, 'rounded-t-md mb-2')} square={true}>
+            {items.map(({icon, title, badge, content}, index) => (
+                <MuiAccordion elevation={0} key={index} classes={classes} className={clsx(classes.root, 'rounded-t-md mb-2')} square={true} expanded={expanded === index} onChange={handleChange(index)}>
                     <MuiAccordionSummary
                         expandIcon={<ExpandMoreIcon className={clsx(colorClass({color, variant}))} />}
                         aria-controls={`panel${index}-content`} id={`panel${index}-header`}
-                        className={clsx('rounded-t-md', colorClass({color, variant}))}
-                    >
-                        <div className={'flex flex-row items-center space-x-2'}>
-                            {icon && <Icon icon={icon} />}
-                            <div className={'font-bold'}>{title}</div>
+                        className={clsx('rounded-t-md', colorClass({color, variant}))}>
+                        <div className={'w-full flex justify-between items-center'}>
+                            <div className={'flex justify-between items-center space-x-2'}>
+                                {icon && <Icon icon={icon} />}
+                                <div className={'font-bold'}>{title}</div>
+                            </div>
+                            {badge && <Badge color={badge.color} variant={badge.type} text={badge.label} />}
                         </div>
                     </MuiAccordionSummary>
                     <MuiAccordionDetails className={'py-3 px-4'}>
-                        <div className={'text-sm tracking-wider'}>{content}</div>
+                        {typeof content === 'string' && <div className={'text-sm tracking-wider'}>{content}</div>}
+                        {Array.isArray(content) &&
+                            <div className={'flex flex-col space-y-4'}>
+                                {content.map(({label, target}, index) => (
+                                    <a className={'text-gray-400'} href={target}>-  {label}</a>
+                                ))}
+                            </div>
+                        }
                     </MuiAccordionDetails>
                 </MuiAccordion>
             ))}
@@ -42,7 +55,7 @@ export function Accordion({items = [], color, variant = 'contained'}: AccordionP
 }
 
 export interface AccordionProps {
-    items: {icon?: string, title?: string, content?: string}[]
+    items: {icon?: string, title?: string, badge?: {type: badge_variant, label: string, color: box_color}, content?: string | any[]}[]
     color?: box_color,
     variant?: box_variant,
 }
