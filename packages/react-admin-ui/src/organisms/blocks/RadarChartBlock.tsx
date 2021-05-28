a9d4790a625a225d3c499e79b3b52d977eeacdf7
carl-mrchnd <66088573+carl-mrchnd@users.noreply.github.com>
2021-05-28 08:40:30 +0200

    feat: CU-jhba5q create radar chart block (#40)

--- packages/react-admin-ui/src/organisms/blocks/RadarChartBlock.tsx
+++ packages/react-admin-ui/src/organisms/blocks/RadarChartBlock.tsx
@@ -1,10 +1,19 @@
-export function RadarChartBlock({}: RadarChartBlockProps) {
+import {BaseBlockProps, Block} from "../../atoms";
+import {RadarChart} from "../../molecules";
+
+export function RadarChartBlock({title, dropdownItems, labels, series, ...props}: RadarChartBlockProps) {
     return (
-        <div>RADAR CHART BLOCK</div>
+        <Block padding={'none'} title={title} {...props} dropdownItems={dropdownItems}>
+            <RadarChart color={props.color} variant={props.variant} isMenu={false} labels={labels} series={series} />
+        </Block>
     );
 }
 
-export interface RadarChartBlockProps {
+export interface RadarChartBlockProps extends BaseBlockProps {
+    title?: string,
+    dropdownItems?: {name?: string}[],
+    labels?: string[],
+    series?: {name?: string, data?: number[]}[],
 }
 
 export default RadarChartBlock
