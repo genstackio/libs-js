7d50c6cf15e88ed8b532f2fc7a043b9a0afe656a
Xavier GENOLHAC <70317658+koala819@users.noreply.github.com>
2021-06-04 22:55:40 +0200

    feat : CU-kz6yxc create Pricing (#66)

---
+++ packages/react-admin-ui/__stories__/molecules/Pricing.stories.tsx
@@ -0,0 +1,193 @@
+import {args, s} from '../utils';
+import {Pricing} from '../../src';
+
+export default {
+    title: 'Molecules/Pricing',
+    component: Pricing,
+    argTypes: {
+        items: args.items,
+    },
+
+}
+
+const Template = args => (<Pricing {...args} />);
+
+export const basic = s(Template, {
+    items: [
+        {
+            features: [
+                {
+                    title: "50GB Disk Space",
+                },
+                {
+                    title: "50 Email Accounts",
+                },
+                {
+                    title: "Maintenance",
+                },
+                {
+                    title: "15 Subdomains",
+                }],
+            label: "Sign Up",
+            name: "standard",
+            price: 10,
+            variant: 'contained',
+        },
+        {
+            features: [
+                {
+                    title: "10 % on all product",
+                },
+                {
+                    title: "50 Email Accounts",
+                },
+                {
+                    title: "Maintenance",
+                },
+                {
+                    title: "15 Subdomains",
+                }],
+            label: "Sign Up",
+            name: "prenium",
+            price: 20,
+            variant: 'contained',
+        },
+        {
+            features: [
+                {
+                    title: "Upload 50 products",
+                },
+                {
+                    title: "50 Email Accounts",
+                },
+                {
+                    title: "Maintenance",
+                },
+                {
+                    title: "15 Subdomains",
+                }],
+            label: "Sign Up",
+            name: "auther pack",
+            price: 50,
+            variant: 'contained',
+        },{
+            features: [
+                {
+                    title: "Upload 50 products",
+                },
+                {
+                    title: "50 Email Accounts",
+                },
+                {
+                    title: "Maintenance",
+                },
+                {
+                    title: "15 Subdomains",
+                }],
+            label: "Sign Up",
+            name: "auther pack",
+            price: 50,
+            variant: 'contained',
+        },
+    ],
+})
+export const withBackgroundImage = s(Template, {
+    items: [
+        {
+            currency: '$',
+            features: [
+                {
+                    title: "50GB Disk Space",
+                },
+                {
+                    title: "50 Email Accounts",
+                },
+                {
+                    title: "Maintenance",
+                },
+                {
+                    title: "15 Subdomains",
+                }],
+            image: {
+                url: 'https://images.unsplash.com/photo-1557682268-e3955ed5d83f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=486&q=80',
+            },
+            label: "Sign Up",
+            name: "standard",
+            period: '/mo',
+            price: 10,
+            variant: 'contained',
+        },
+        {
+            currency: '$',
+            features: [
+                {
+                    title: "10 % on all product",
+                },
+                {
+                    title: "50 Email Accounts",
+                },
+                {
+                    title: "Maintenance",
+                },
+                {
+                    title: "15 Subdomains",
+                }],
+            image: {
+                url: 'https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=486&q=80',
+            },
+            label: "Sign Up",
+            name: "prenium",
+            period: '/mo',
+            price: 20,
+            variant: 'contained',
+        },
+        {
+            currency: '$',
+            features: [
+                {
+                    title: "Upload 50 products",
+                },
+                {
+                    title: "50 Email Accounts",
+                },
+                {
+                    title: "Maintenance",
+                },
+                {
+                    title: "15 Subdomains",
+                }],
+            image: {
+                url: 'https://images.unsplash.com/photo-1557682268-e3955ed5d83f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=486&q=80',
+            },
+            label: "Sign Up",
+            name: "auther pack",
+            period: '/mo',
+            price: 50,
+            variant: 'contained',
+        },
+        {
+            currency: '$',
+            features: [
+                {
+                    title: "Upload 50 products",
+                },
+                {
+                    title: "50 Email Accounts",
+                },
+                {
+                    title: "Maintenance",
+                },
+                {
+                    title: "15 Subdomains",
+                }],
+            image: {
+                url: 'https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=486&q=80',
+            },
+            label: "Sign Up",
+            name: "auther pack",
+            period: '/mo',
+            price: 50,
+            variant: 'contained',
+        },
+    ],
+})
---
+++ packages/react-admin-ui/src/molecules/Pricing.tsx
@@ -0,0 +1,40 @@
+import {MouseEventHandler} from "react";
+import clsx from "clsx";
+import Block from '../atoms/Block';
+import Button from "../atoms/Button";
+import colorClass from "../utils/colorClass";
+import Text from "../atoms/Text";
+import {box_color, box_variant, image} from "../types";
+
+export function Pricing({items}: PricingProps) {
+    return (
+        <div className='flex inline'>
+            {items && items.map((item, index) => (
+                <Block image={item.image} classes={{content: 'space-y-3 flex justify-center items-center flex-col pr-4 pl-4'}}>
+                    <div key={index} className='uppercase'>
+                        {item && item.name && <Text text={item.name} variant='title4'/>}
+                    </div>
+                    <div className={clsx(colorClass({color: item.color, variant: item.variant}), 'flex rounded-full h-32 w-32 items-center justify-center')}>
+                        <Text text={item.currency} variant='description'/>
+                        <Text text={`${item.price}`} variant='title1'/>
+                        <Text text={item.period} variant='subtitle'/>
+                    </div>
+                    <div className="flex flex-col justify-center space-y-3">
+                        {item.features && item.features.map(({title}, index) => (
+                            <Text key={index} text={title} variant='subtitle'/>
+                        ))}
+                    </div>
+                    <div className='uppercase'>
+                        <Button variant={item.variant} color={item.color} onClick={item.onClick}>{item.label}</Button>
+                    </div>
+                </Block>
+            ))}
+        </div>
+    );
+}
+
+export interface PricingProps {
+    items: {color?: box_color, currency?: string, features?: {title?: string}[], image?: image, label?: string, name: string, onClick: MouseEventHandler, period: string, price: number, variant?: box_variant}[],
+}
+
+export default Pricing
