a52473a3d267e0ee8dd08c782c4dca115307328e
AugustinBoju <bojuaugustin@gmail.com>
2021-06-08 14:01:48 +0200

    feat: CU-m3dqc9_Composant-Molecules--Card

---
+++ packages/react-admin-ui/__stories__/molecules/Card.stories.tsx
@@ -0,0 +1,30 @@
+import {args, s} from "../utils";
+import {Card} from "../../src";
+
+export default {
+    title: 'Molecules/Card',
+    component: Card,
+    argTypes: {
+        title: args.title,
+        description: args.text,
+        color: args.color,
+        variant: args.blockVariant,
+        onClick: args.onClick,
+    },
+}
+
+const Template = args => <Card {...args} />
+
+export const basic = s(Template, {
+    image: {
+        url: 'https://picsum.photos/1900/500',
+        alt: 'image',
+    },
+    title: "Web Design",
+    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    content: "Dec 15, 2019",
+    value: 5,
+    link: "/",
+    variant: "contained",
+    color: "light,"
+});
