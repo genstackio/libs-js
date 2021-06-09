4d6fa36a1d74242c32177be5db7ab67664050381
Augustin <bojuaugustin@gmail.com>
2021-06-09 08:57:39 +0200

    feat: CU-kz6vh8_Composant-Organisms--Blocks--VisualBulletBlock (#68)

--- packages/react-admin-ui/src/molecules/BulletPoint.tsx
+++ packages/react-admin-ui/src/molecules/BulletPoint.tsx
@@ -1,5 +1,4 @@
 import {ReactNode} from 'react';
-import clsx from "clsx";
 import Icon from "../atoms/Icon";
 import {BaseBlockProps, Block} from "../atoms";
 import {box_variant, box_color} from '../types';
@@ -7,14 +6,12 @@ import {box_variant, box_color} from '../types';
 export function BulletPoint({title, text, icon, ...props}: BulletPointProps) {
     return (
         <Block {...props}>
-            <div className={clsx('rounded-2xl shadow-block')}>
-                <div className='p-5 flex items-center'>
+                <div className='flex items-center'>
                     {icon && <Icon icon={icon} />}
-                    <div className='flex-column p-3'>
-                        <h6 className={'mt-4 text-2xl font-bold'}>{title}</h6>
+                    <div className='flex-column ml-4'>
+                        <h6 className={'text-2xl font-bold'}>{title}</h6>
                         <p className=''>{text || ''}</p>
                     </div>
-                </div>
             </div>
         </Block>
     );
