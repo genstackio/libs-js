9dd764d0fb24c07cdae2805b4857b35998939546
carl-mrchnd <66088573+carl-mrchnd@users.noreply.github.com>
2021-06-09 10:49:26 +0200

    feat: CU-m3dybe create Error Template (#76)

---
+++ packages/react-admin-ui/__stories__/templates/ErrorTemplate.stories.tsx
@@ -0,0 +1,27 @@
+import {args, s} from "../utils";
+import {ErrorTemplate} from '../../src';
+
+export default {
+    title: 'Templates/ErrorTemplate',
+    component: ErrorTemplate,
+    argTypes: {
+        code: args.code,
+        message: args.message,
+        actions: args.actions,
+        icon: args.icon,
+        color: args.color,
+    },
+}
+
+const Template = args => <ErrorTemplate {...args} />
+
+export const basic = s(Template, {
+    code: 400,
+    message: 'The page you are attempting to reach is currently not available. This may be because the page does not exist or has been moved.',
+    actions: [{
+        label: 'BACK TO HOME PAGE',
+        target: '/',
+    }],
+    icon: 'sentiment_very_dissatisfied',
+    color: 'primary',
+});
--- packages/react-admin-ui/src/templates/index.ts
+++ packages/react-admin-ui/src/templates/index.ts
@@ -1,2 +1,3 @@
 export * from './layouts';
 export * from './LoginTemplate';
+export * from './ErrorTemplate';
