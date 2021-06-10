7442478dc6e3f7b535da4f2352169022a0d8c67a
carl-mrchnd <66088573+carl-mrchnd@users.noreply.github.com>
2021-06-10 13:16:39 +0200

    feat: CU-m3e1yq create Login2Template (#79)

--- packages/react-admin-ui/src/templates/LoginTemplate.tsx
+++ packages/react-admin-ui/src/templates/LoginTemplate.tsx
@@ -5,12 +5,10 @@ import {Container} from "../atoms";
 
 export function LoginTemplate({image, logo, form}: LoginTemplateProps) {
     return (
-        <Container bgImage={image} className={'bg-cover h-screen flex justify-center items-center py-10 px-3'}>
-            <div className={'space-y-4'}>
-                {logo && <Image url={logo.url} alt={logo.alt} className={'mx-auto'} />}
-                <div className={'p-10 xs:p-5 bg-white max-w-xs'}>
-                    {form}
-                </div>
+        <Container bgImage={image} className={'bg-cover h-screen py-10 px-3 flex flex-col justify-center items-center'}>
+            {logo && <Image {...logo} className={'mx-auto'} />}
+            <div className={'mt-4 p-10 xs:p-5 w-full xs:w-min max-w-md bg-white rounded-lg shadow-block'}>
+                {form || ''}
             </div>
         </Container>
     );
