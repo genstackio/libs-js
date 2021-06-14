93718770b9d31100d649664ead58fa731d89871d
AugustinBoju <bojuaugustin@gmail.com>
2021-06-14 10:19:18 +0200

    fix: CU-mdcj15_Refactoring--Grer-leffet-de-stretching-sur-la-page-de-login-pour-la-bgimage

--- packages/react-admin-ui/src/templates/Login2Template.tsx
+++ packages/react-admin-ui/src/templates/Login2Template.tsx
@@ -5,17 +5,17 @@ import {Container} from "../atoms";
 
 export function Login2Template({image, logo, bgColor, form}: Login2TemplateProps) {
     return (
-        <div className={'h-screen flex bg-opacity-70 sm:relative sm:items-center'}>
-            <Container bgImage={image} className={'w-7/12 p-3 bg-cover sm:absolute sm:w-full sm:h-full sm:opacity-80'} />
-            <Container bgColor={bgColor} className={'w-5/12 py-8 px-3 flex flex-col justify-center items-center sm:hidden'}>
+        <div className={'h-screen flex bg-opacity-70 md:relative md:items-center sm:relative sm:items-center'}>
+            <Container bgImage={image} className={'w-7/12 p-3 bg-cover bg-center flex-3 md:opacity-60 md:absolute md:w-full md:h-full sm:opacity-60 sm:absolute sm:w-full sm:h-full'} />
+            <Container bgColor={bgColor} className={'w-5/12 py-8 px-3 flex-2 flex-col justify-center items-center md:hidden sm:hidden'}>
                 {logo && <Image {...logo} />}
                 <div className={'mt-4 p-10 w-full max-w-md bg-white rounded-lg shadow-block'}>
                     {form || ''}
                 </div>
             </Container>
-            <div className={'z-50 hidden sm:block mx-auto'}>
+            <div className={'z-50 hidden flex sm:block md:block mx-auto'}>
                 {logo && <Image {...logo} />}
-                <div className={'mt-4 p-10 xs:p-5 w-full xs:w-min bg-white rounded-lg shadow-block'}>
+                <div className={'mt-4 p-10 xs:p-5 md:p-5 w-full bg-white rounded-lg shadow-block'}>
                     {form || ''}
                 </div>
             </div>
