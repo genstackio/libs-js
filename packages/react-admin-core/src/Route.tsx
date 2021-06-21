ddf11605a7091aa2804abffaf996730e1dba01c7
Olivier Hoareau <oha@greenberets.io>
2021-06-21 09:09:40 +0200

    fix(react-contexts): hooks

--- packages/react-admin-core/src/Route.tsx
+++ packages/react-admin-core/src/Route.tsx
@@ -1,10 +1,10 @@
 import {Route as BaseRoute} from "react-router-dom";
 import {route} from "./types";
-import screenFactory from "./utils/screenFactory";
+import componentFactory from "./utils/componentFactory";
 
 export function Route({screenImporter, user = undefined, secured = true, routes = [], path, exact = true, component = undefined, screen: screenName}: route) {
     screenName = (secured && !user) ? 'login' : screenName;
-    const Component = (screenName && screenImporter) ? screenFactory(screenName, screenImporter) : component;
+    const Component = (screenName && screenImporter) ? componentFactory(screenName, screenImporter) : component;
     return (
         <BaseRoute
             {...(path ? {path} : {})}
--- packages/react-admin-core/src/utils/screenFactory.ts
+++
@@ -1,7 +0,0 @@
-import componentFactory from "./componentFactory";
-
-export function screenFactory(name: string, importer: (name: string) => any) {
-    return componentFactory(name, importer, 'Screen');
-}
-
-export default screenFactory
