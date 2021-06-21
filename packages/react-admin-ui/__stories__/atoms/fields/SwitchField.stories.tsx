8d85643e6ca2deb6d96cee6bcc380d48e1a97b20
carl-mrchnd <66088573+carl-mrchnd@users.noreply.github.com>
2021-06-21 12:16:09 +0200

    feat: CU-mq9dh2 create Switch (#107)
    Co-authored-by: Olivier Hoareau <oha@greenberets.io>

---
+++ packages/react-admin-ui/__stories__/atoms/fields/SwitchField.stories.tsx
@@ -0,0 +1,24 @@
+import {args, s} from "../../utils";
+import {SwitchField} from '../../../src';
+
+export default {
+    title: 'Atoms/fields/SwitchField',
+    component: SwitchField,
+    argTypes: {
+        label: args.label,
+        disabled: args.disabled,
+        required: args.required,
+        helper: args.helper,
+        name: args._disable,
+        register: args._disable,
+        errors: args._disable,
+        defaults: args._disable,
+        onChange: args._disable,
+        options: args._disable,
+        field: args._disable,
+    },
+}
+
+const Template = args => <SwitchField {...args} />;
+
+export const basic = s(Template, {})
