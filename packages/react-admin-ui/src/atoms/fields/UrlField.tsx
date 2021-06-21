88a83b1c83e3034aaab5283fcb811e40e7c1b170
koala819 <x.genolhac@gmail.com>
2021-06-21 10:57:34 +0200

    feat: CU-mq982k create UrlField

---
+++ packages/react-admin-ui/src/atoms/fields/UrlField.tsx
@@ -0,0 +1,25 @@
+import {useTranslation} from "react-i18next";
+import {useMemo} from "react";
+import TextField, {TextFieldProps} from "./TextField";
+
+export function UrlField({options = {}, ...props}: UrlFieldProps) {
+    const {t} = useTranslation();
+    options = useMemo(() => ({
+        ...options,
+        pattern: {
+            value: /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/,
+            message: t('constraints_url')
+        },
+        minLength: 3,
+        maxLength: 255,
+    }), [options]);
+
+    return (
+        <TextField name={'URL'} {...props} options={options} />
+    );
+}
+
+export interface UrlFieldProps extends TextFieldProps {
+}
+
+export default UrlField
