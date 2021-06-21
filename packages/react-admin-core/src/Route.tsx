e320d8b53decab997032cdf86e25cc84cb906318
Olivier Hoareau <oha@greenberets.io>
2021-06-21 10:19:15 +0200

    fix: optimization to avoid rerender

--- packages/react-admin-core/src/Route.tsx
+++ packages/react-admin-core/src/Route.tsx
@@ -1,18 +1,20 @@
 import {Route as BaseRoute} from "react-router-dom";
 import {route} from "./types";
 import componentFactory from "./utils/componentFactory";
+import {useCallback, useMemo} from "react";
 
 export function Route({screenImporter, user = undefined, secured = true, routes = [], path, exact = true, component = undefined, screen: screenName}: route) {
     screenName = (secured && !user) ? 'login' : screenName;
-    const Component = (screenName && screenImporter) ? componentFactory(screenName, screenImporter) : component;
+    const Component = useMemo(() => (screenName && screenImporter) ? componentFactory(screenName, screenImporter) : component, [screenName, screenImporter, component]);
+    const render = useCallback((props: any) => (
+        // pass the sub-routes down to keep nesting
+        <Component {...props} routes={routes} />
+    ), [routes, Component]);
     return (
         <BaseRoute
             {...(path ? {path} : {})}
             {...(('boolean' === typeof exact) ? {exact} : {})}
-            render={(props: any) => (
-                // pass the sub-routes down to keep nesting
-                <Component {...props} routes={routes} />
-            )}
+            render={render}
         />
     );
 }
