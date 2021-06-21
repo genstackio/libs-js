f201d956247963ab84feb1afc56cd0e106ca85cb
Xavier GENOLHAC <70317658+koala819@users.noreply.github.com>
2021-06-21 12:17:36 +0200

    feat: CU-mq982k create UrlField (#108)
    * feat: CU-mq982k create UrlField
    
    * Update UrlField.tsx
    
    Co-authored-by: Olivier Hoareau <oha@greenberets.io>

---
+++ packages/react-admin-ui/__stories__/atoms/fields/UrlField.stories.tsx
@@ -0,0 +1,23 @@
+import {args, s} from "../../utils";
+import {UrlField} from '../../../src';
+
+export default {
+    title: 'Atoms/fields/UrlField',
+    component: UrlField,
+    argTypes: {
+        disabled: args.disabled,
+        label: args.label,
+        placeholder: args.placeholder,
+        register: args._disable,
+        type: args.fieldType,
+        errors: args._disable,
+        defaults: args._disable,
+        onChange: args._disable,
+        options: args._disable,
+        value: args.fieldValue,
+    },
+}
+
+const Template = args => <UrlField {...args} />;
+
+export const basic = s(Template, {})
