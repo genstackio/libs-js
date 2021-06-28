8616169cd8a83c5974a191173110f8869d9cd8fc
Olivier Hoareau <oha@greenberets.io>
2021-06-28 14:27:58 +0200

    chore: fix format

--- packages/react-admin-core/src/configs/translations.ts
+++ packages/react-admin-core/src/configs/translations.ts
@@ -2,6 +2,7 @@ export default {
     'fr-FR': {
         translation: {
             project_name: 'React Admin Core',
+            screen_notfound_message: "La page demandée n'existe pas ou a été supprimé",
         },
     },
 };
