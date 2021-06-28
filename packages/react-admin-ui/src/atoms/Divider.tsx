c249e4a6c4f7f4cd6a10bd561e8a8b24e6d65b4d
Olivier Hoareau <oha@greenberets.io>
2021-06-28 11:59:58 +0200

    Update Divider.tsx

--- packages/react-admin-ui/src/atoms/Divider.tsx
+++ packages/react-admin-ui/src/atoms/Divider.tsx
@@ -1,17 +1,19 @@
+import {class_name} from '../types';
 import {box_color} from "../mappings/box-colors";
 import clsx from "clsx";
-import buttonClass from "../utils/buttonClass";
+import boxClass from "../utils/boxClass";
 import {box_variant} from "../mappings/box-variants";
 
-export function Divider({color, variant='outlined' }: DividerProps) {
+export function Divider({className, color, variant='outlined' }: DividerProps) {
     return (
-        <hr className={clsx(buttonClass({ color, variant }))} />
+        <hr className={clsx(boxClass({ color, variant }), className)} />
     );
 }
 
 export interface DividerProps {
+    className?: class_name;
     color?: box_color;
-    variant?: box_variant,
+    variant?: box_variant;
 }
 
 export default Divider;
