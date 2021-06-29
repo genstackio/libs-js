986935a8444c7e6266c541771d866135e4c562aa
Federica Buzzi <infofedericabuzzi@gmail.com>
2021-06-29 16:05:17 +0200

    wip

--- packages/figma-plugin-react-admin/src/actions/text.ts
+++ packages/figma-plugin-react-admin/src/actions/text.ts
@@ -1,10 +1,10 @@
 import FigmaService from "../services/FigmaService";
 
-export function text({value}: any, {figma}: {figma: FigmaService}) {
+export async function text({value}: any, {figma}: {figma: FigmaService}) {
+    await figma.getApi().loadFontAsync({family:"Roboto", style:"Regular"})
     const t = figma.getApi().createText();
-
+    t.fontName = {family:"Roboto", style:"Regular"};
     t.characters = value as string;
-
     figma.getApi().currentPage.appendChild(t);
 }
 
