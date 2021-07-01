7db04cba6fab9493bb325391f2d7c775c77c2d7c
Augustin <bojuaugustin@gmail.com>
2021-07-01 13:29:06 +0200

    feat: CU-9txjub_Composant--Atoms--NotificationToolbarItem (#141)

---
+++ packages/react-admin-ui/src/atoms/NotificationToolbarItem.tsx
@@ -0,0 +1,24 @@
+import { Icon } from './';
+import { class_name } from '../types';
+import { useToggle } from '../hooks/useToggle';
+import clsx from 'clsx';
+
+export function NotificationToolbarItem({ className }: NotificationToolbarItemProps) {
+    const [active, toggle] = useToggle();
+    const color = active ? 'danger' : 'inherit';
+
+    return (
+        <Icon
+            icon={'notifications_icon'}
+            color={color}
+            onClick={toggle}
+            className={clsx('cursor-pointer', className)}
+        />
+    );
+}
+
+export interface NotificationToolbarItemProps {
+    className?: class_name;
+}
+
+export default NotificationToolbarItem;
