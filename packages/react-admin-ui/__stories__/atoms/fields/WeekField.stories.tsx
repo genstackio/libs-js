4189b72cb00137b2fe84c95fca2ffa4cd48a63ce
koala819 <x.genolhac@gmail.com>
2021-07-05 16:31:26 +0200

    feat: CU-mq99am create WeekField

---
+++ packages/react-admin-ui/__stories__/atoms/fields/WeekField.stories.tsx
@@ -0,0 +1,24 @@
+import { a, args, s } from '../../utils';
+import { WeekField } from '../../../src';
+
+export default {
+    title: 'Atoms/fields/WeekField',
+    component: WeekField,
+    argTypes: a({
+        disabled: args.disabled,
+        label: args.label,
+        placeholder: args.placeholder,
+        register: args._disabled,
+        type: args.fieldType,
+        errors: args._disabled,
+        defaults: args._disabled,
+        onChange: args._disabled,
+        options: args._disabled,
+        value: args.fieldValue,
+        kind: args._disabled,
+    }),
+};
+
+const Template = (args) => <WeekField {...args} />;
+
+export const basic = s(Template, {});
