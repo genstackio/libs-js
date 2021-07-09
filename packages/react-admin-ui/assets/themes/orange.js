7696f97fe6dd41070869a7fff2d5684314628a92
Federica Buzzi <infofedericabuzzi@gmail.com>
2021-07-09 14:39:26 +0200

    more color themes

---
+++ packages/react-admin-ui/assets/themes/orange.js
@@ -0,0 +1,21 @@
+module.exports = {
+    color_primary: '#F3722C',
+    color_secondary: '#43AA8B',
+    color_success: '#90BE6D',
+    color_info: '#577590',
+    color_warning: '#F9C74F',
+    color_danger: '#F94144',
+    color_dark: '#2E294E',
+    color_light: '#EADEDA',
+    color_clear: '#FFFFFF',
+    // dark
+    dark_color_primary: '#F3722C',
+    dark_color_secondary: '#43AA8B',
+    dark_color_success: '#90BE6D',
+    dark_color_info: '#577590',
+    dark_color_warning: '#F9C74F',
+    dark_color_danger: '#F94144',
+    dark_color_dark: '#2E294E',
+    dark_color_light: '#EADEDA',
+    dark_color_clear: '#262932',
+};
