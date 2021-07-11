815812cf3c9ac02bd2684698b85a39c2633fe369
Olivier Hoareau <oha@greenberets.io>
2021-07-11 17:44:59 +0200

    feat: image enhancements

--- packages/react-admin-ui/src/atoms/Image.tsx
+++ packages/react-admin-ui/src/atoms/Image.tsx
@@ -1,16 +1,36 @@
+import {useMemo} from 'react';
 import { class_name } from '../types';
 import clsx from 'clsx';
 import cornerClass, { corner } from '../mappings/corners';
 
-export function Image({ alt, className, url, corner = 'square' }: ImageProps) {
+const defaultImage = 'https://via.placeholder.com/350x250?text=%20';
+
+export function Image({className, url, corner = 'square', loading = 'lazy', alt, tablet, mobile, objectFit = 'initial', objectPosition = undefined}: ImgProps) {
+    const style = useMemo(() => ({objectFit, objectPosition}), [objectFit, objectPosition]);
     if (!url) return null;
-    return <img src={url} alt={alt} className={clsx(cornerClass(corner), className)} />;
+    return (
+        <picture>
+            {mobile && mobile.url && <source media="(max-width: 600px)" srcSet={mobile.url}/>}
+            {tablet && tablet.url && <source media="(max-width: 960px)" srcSet={tablet.url}/>}
+            <source media="(min-width: 961px)" srcSet={url || defaultImage}/>
+            <img className={clsx('w-full h-full', cornerClass(corner), className)}
+                 alt={alt} style={style} src={url || defaultImage}
+                 loading={loading}
+            />
+        </picture>
+    );
 }
 
-export interface ImageProps {
+export interface ImgProps {
     className?: class_name;
-    alt: string;
-    url: string;
+    url?: string,
+    alt?: string,
+    tablet?: any,
+    mobile?: any,
+    objectFit?: any,
+    objectPosition?: any,
+    rounded?: boolean,
+    loading?: 'lazy'
     corner?: corner;
 }
 
