8e22dda79c7670cff2ab255bae4e25d398c9cd52
Olivier Hoareau <oha@greenberets.io>
2021-07-19 12:19:35 +0200

    feat(pdf-generator): CU-adz6wv traits de coupe

--- packages/pdf-generator/src/index.tsx
+++ packages/pdf-generator/src/index.tsx
@@ -1,8 +1,9 @@
 import ReactPDF from '@react-pdf/renderer';
 import PdfDocument from './PdfDocument';
+import path from 'path';
 
 async function cli(configFile, targetFile) {
-    await ReactPDF.render(<PdfDocument />, targetFile);
+    await ReactPDF.render(<PdfDocument config={require(path.resolve(configFile))} />, targetFile);
 }
 
 export * from './pages';
