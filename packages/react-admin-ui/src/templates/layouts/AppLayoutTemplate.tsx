62fd87461a3af87b545a05109d2b98ea5e0e947f
Olivier Hoareau <oha@greenberets.io>
2021-07-21 23:52:51 +0200

    fix: fix

--- packages/react-admin-ui/src/templates/layouts/AppLayoutTemplate.tsx
+++ packages/react-admin-ui/src/templates/layouts/AppLayoutTemplate.tsx
@@ -15,27 +15,20 @@ export function AppLayoutTemplate({ logo, menu, children, toolbar, ...props }: A
     }, [setShow, show]);
     return (
         <div className={'flex h-screen'}>
-            <div className={clsx('w-drawer bg-clear', !show && 'hidden')}>
-                <div className={'px-8 py-6 h-xxl shadow-toolbar flex justify-between items-center'}>
-                    {logo && <Image className={'bg-auto'} {...logo} />}
-                    <Icon
-                        icon={'apps_outlined'}
-                        className={textClass({ color: props.color, variant: 'light', hoverable: true })}
-                        onClick={handleClick}
-                    />
+            <div className={clsx('w-drawer bg-clear transform h-full overflow-auto transition-all duration-300 z-30 absolute', show ? 'translate-x-0' : '-translate-x-full' )}>
+                <div className={'px-4 py-6 h-xxl shadow-toolbar flex items-center'}>
+                    {logo && <Image {...logo} />}
                 </div>
                 {menu && <Menu items={menu} {...props} />}
             </div>
-            <div className={'flex-1'}>
+            <div className={clsx('flex-1 transition-all duration-300 ', show ? 'x-pl-drawer' : 'pl-0')}>
                 <div className={'px-8 py-6 h-xxl flex items-center'}>
-                    {!show && (
                         <Clickable onClick={handleClick} className={'mr-4'}>
                             <Icon
                                 icon={'menu'}
                                 className={textClass({ color: props.color, variant: 'light', hoverable: true })}
                             />
                         </Clickable>
-                    )}
                     {toolbar || ''}
                 </div>
                 <div className={'h-screen p-4 shadow-inside'}>{children || ''}</div>
