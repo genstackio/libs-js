09c34fa0c576fd62ef649d9861b89adaefae728f
Olivier Hoareau <oha@greenberets.io>
2021-07-21 22:45:08 +0200

    fix(react-admin-ui): login template logo width

--- packages/react-admin-ui/src/templates/LoginTemplate.tsx
+++ packages/react-admin-ui/src/templates/LoginTemplate.tsx
@@ -11,7 +11,7 @@ export function LoginTemplate({ image, logo, form }: LoginTemplateProps) {
                 'bg-cover h-screen py-10 px-3 xs:px-0 flex flex-col justify-center xs:justify-start items-center'
             }
         >
-            {logo && <Image expand={false} {...logo} className={'mx-auto'} />}
+            {logo && <Image expand={false} {...logo} className={'mx-auto max-w-xs xs:max-w-full xs:mx-4'} />}
             <div
                 className={
                     'mt-4 p-10 xs:p-5 w-full max-w-md xs:max-w-full bg-clear rounded-lg xs:rounded-none shadow-block'
