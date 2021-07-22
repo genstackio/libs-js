cedda01b24692c5771c000a2719c8adacd6581b4
cnote <56361858+mhcini@users.noreply.github.com>
2021-07-22 10:32:49 +0200

    feat(pdf-generator): CU-ae4etw json variables template width height (#172)

--- packages/pdf-generator/src/PdfDocument.tsx
+++ packages/pdf-generator/src/PdfDocument.tsx
@@ -1,13 +1,13 @@
 import { Document } from '@react-pdf/renderer';
 import * as pages from './pages';
 
-export function PdfDocument({config = {}}: PdfDocumentProps) {
+export function PdfDocument({ config = {} }: PdfDocumentProps) {
     const template = config['template'] || 'default';
     const componentName = `${template.slice(0, 1).toUpperCase()}${template.slice(1)}Page`;
     const Component = pages[componentName] || pages['DefaultPage'];
     return (
         <Document>
-            <Component />
+            <Component {...config} />
         </Document>
     );
 }
