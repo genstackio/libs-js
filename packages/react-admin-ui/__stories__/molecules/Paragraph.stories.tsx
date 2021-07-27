e14bd4caf0d74de9fc8a1306c4814e1a0a34296d
cnote <56361858+mhcini@users.noreply.github.com>
2021-07-27 09:04:49 +0200

    feat(react-admin-ui): Molecules > Paragraph done (#181)
    * feat(react-admin-ui): Molecules > Paragraph done
    
    * feat(react-admin-ui): Molecules > Paragraph done pt.2

---
+++ packages/react-admin-ui/__stories__/molecules/Paragraph.stories.tsx
@@ -0,0 +1,38 @@
+import { s, a, args } from '../utils';
+import { Paragraph } from '../../src';
+
+export default {
+    title: 'Molecules/Paragraph',
+    component: Paragraph,
+    argTypes: a({
+        title: args.title,
+        text: args.text,
+        color: args.color,
+        btnLabel: args.btnLabel,
+        btnTarget: args.target,
+        image: { control: { type: 'object' } },
+        imageLeft: { control: { type: 'boolean' } },
+    }),
+};
+
+const Template = (args) => <Paragraph {...args} />;
+
+export const basic = s(Template, {
+    title: 'Bienvenue chez Gotombola',
+    text: "Nous voulons permettre à toutes les associations de s'auto-financer rapidement et facilement grâce à la tombola augmentée par le digital. Pour aider un maximum d'associations, nous ne pensons pas que la rupture 100% digitale soit adaptée. Nous vous accompagnons en combinant outils digitaux et traditionnels pour ne mettre personne de côté. Commencez à promouvoir votre tombola plus efficacement en créant gratuitement votre sticker digital GoTombola.",
+});
+
+export const withImage = s(Template, {
+    title: 'Bienvenue chez Gotombola',
+    text: "Nous voulons permettre à toutes les associations de s'auto-financer rapidement et facilement grâce à la tombola augmentée par le digital. Pour aider un maximum d'associations, nous ne pensons pas que la rupture 100% digitale soit adaptée. Nous vous accompagnons en combinant outils digitaux et traditionnels pour ne mettre personne de côté. Commencez à promouvoir votre tombola plus efficacement en créant gratuitement votre sticker digital GoTombola.",
+    image: { url: 'https://via.placeholder.com/687x426?text=Image' },
+    imageLeft: true,
+});
+
+export const withButton = s(Template, {
+    title: 'Bienvenue chez Gotombola',
+    text: "Nous voulons permettre à toutes les associations de s'auto-financer rapidement et facilement grâce à la tombola augmentée par le digital. Pour aider un maximum d'associations, nous ne pensons pas que la rupture 100% digitale soit adaptée. Nous vous accompagnons en combinant outils digitaux et traditionnels pour ne mettre personne de côté. Commencez à promouvoir votre tombola plus efficacement en créant gratuitement votre sticker digital GoTombola.",
+    image: { url: 'https://via.placeholder.com/687x426?text=Image' },
+    btnLabel: 'About Us',
+    btnTarget: 'https://google.fr',
+});
---
+++ packages/react-admin-ui/src/molecules/Paragraph.tsx
@@ -0,0 +1,50 @@
+import clsx from 'clsx';
+import Button from '../atoms/Button';
+import Image from '../atoms/Image';
+import Row from '../atoms/Row';
+import Text from '../atoms/Text';
+import { box_color, rich_text } from '../types';
+import { WithTitle, WithText, WithImage, WithTextColor, WithBox } from '../withs';
+// @todo: add theme
+
+export function Paragraph({
+    title,
+    text,
+    btnLabel,
+    btnTarget,
+    color = 'primary',
+    imageLeft = false,
+    image,
+}: ParagraphProps) {
+    return (
+        <Row className={clsx('xs:flex-col', imageLeft && 'flex-row-reverse xs:flex-col-reverse')}>
+            <div className={clsx('flex-1')}>
+                <Text className={'mb-2'} variant={'title5'} text={title} color={color} />
+                <div className={'th-title-underline'} />
+                <>
+                    <Text className={'leading-loose py-3'} variant={'body'} text={text} />
+                    <div className={'flex justify-center mt-3'}>
+                        <Button variant={'filled'} color={color} onClick={btnTarget}>
+                            {btnLabel}
+                        </Button>
+                    </div>
+                </>
+            </div>
+            {image && (
+                <div className={'flex-1 mr-4'}>
+                    <Image {...image} />
+                </div>
+            )}
+        </Row>
+    );
+}
+
+export interface ParagraphProps extends WithTitle, WithText, WithImage, WithTextColor, WithBox {
+    btnLabel?: rich_text;
+    btnTarget?: string;
+    imageLeft?: boolean;
+    color?: box_color;
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default Paragraph;
