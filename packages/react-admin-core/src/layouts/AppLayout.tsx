2935d0d25e0cb5fa432360229d80618ceac1dcfd
Olivier Hoareau <oha@greenberets.io>
2021-07-28 15:19:20 +0200

    Update AppLayout.tsx

--- packages/react-admin-core/src/layouts/AppLayout.tsx
+++ packages/react-admin-core/src/layouts/AppLayout.tsx
@@ -2,32 +2,14 @@ import AppLayoutTemplate, {
     AppLayoutTemplateProps,
 } from '@genstackio/react-admin-ui/lib/templates/layouts/AppLayoutTemplate';
 import { useUser } from '@genstackio/react-contexts';
-import AppToolbar from '../AppToolbar';
+import AppToolbar, {AppToolbarProps} from '../AppToolbar';
 import { menu_button_item, flag } from '@genstackio/react-admin-ui/lib/types';
 
 export function AppLayout({
     userMenu,
-    fullScreenEnabled = false,
-    messagesEnabled = false,
-    shoppingCartEnabled = false,
-    darkModeEnabled = false,
-    favoritesEnabled = false,
-    notificationsEnabled = false,
-    searchBarEnabled = false,
-    languageEnabled = false,
-    ...props
+    toolbarProps,
 }: AppLayoutProps) {
     const [user, , logout] = useUser();
-    const toolbarProps = {
-        fullScreenEnabled: fullScreenEnabled,
-        messagesEnabled: messagesEnabled,
-        shoppingCartEnabled: shoppingCartEnabled,
-        darkModeEnabled: darkModeEnabled,
-        favoritesEnabled: favoritesEnabled,
-        notificationsEnabled: notificationsEnabled,
-        searchBarEnabled: searchBarEnabled,
-        languageEnabled: languageEnabled,
-    };
 
     return (
         <AppLayoutTemplate
@@ -39,14 +21,7 @@ export function AppLayout({
 
 export interface AppLayoutProps extends AppLayoutTemplateProps {
     userMenu?: menu_button_item[];
-    fullScreenEnabled?: flag;
-    messagesEnabled?: flag;
-    shoppingCartEnabled?: flag;
-    darkModeEnabled?: flag;
-    favoritesEnabled?: flag;
-    notificationsEnabled?: flag;
-    searchBarEnabled?: flag;
-    languageEnabled?: flag;
+    toolbarProps?: AppToolbarProps;
 }
 
 export default AppLayout;
