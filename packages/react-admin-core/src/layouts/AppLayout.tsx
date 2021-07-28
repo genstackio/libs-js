d4cac060a6d7fe34c591deb59f57c7ec80a5d7fd
Olivier Hoareau <oha@greenberets.io>
2021-07-28 15:25:50 +0200

    Update AppLayout.tsx

--- packages/react-admin-core/src/layouts/AppLayout.tsx
+++ packages/react-admin-core/src/layouts/AppLayout.tsx
@@ -3,7 +3,7 @@ import AppLayoutTemplate, {
 } from '@genstackio/react-admin-ui/lib/templates/layouts/AppLayoutTemplate';
 import { useUser } from '@genstackio/react-contexts';
 import AppToolbar, {AppToolbarProps} from '../AppToolbar';
-import { menu_button_item, flag } from '@genstackio/react-admin-ui/lib/types';
+import { menu_button_item } from '@genstackio/react-admin-ui/lib/types';
 
 export function AppLayout({
     userMenu,
