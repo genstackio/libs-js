a1d0df9796b803fee4c5cb995e0b31f09231a854
mhcini <mounirhcini@gmail.com>
2021-07-28 09:42:15 +0200

    feat(react-admin-ui): CU-ap4w8v  MenuButton prop flag avatar

--- packages/react-admin-ui/__stories__/molecules/MenuButton.stories.tsx
+++ packages/react-admin-ui/__stories__/molecules/MenuButton.stories.tsx
@@ -16,12 +16,59 @@ export default {
 const Template = (args) => <MenuButton {...args} />;
 
 export const basic = s(Template, {
-    image: {
-        url: 'https://picsum.photos/40/40',
-        alt: 'picture',
-    },
     label: 'John Doe',
     description: 'Admin',
+    items: [
+        {
+            icon: 'person',
+            label: 'Account',
+            target: 'https://www.google.com/',
+        },
+        {
+            icon: 'settings',
+            label: 'Settings',
+            target: 'https://www.google.com/',
+        },
+        {
+            icon: 'exit_to_app',
+            label: 'Logout',
+            target: () => alert('Logout'),
+        },
+    ],
+});
+
+export const withImage = s(Template, {
+    label: 'John Doe',
+    description: 'Admin',
+    size: 'xs',
+    image: {
+        url: 'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        alt: 'profile picture',
+    },
+    items: [
+        {
+            icon: 'person',
+            label: 'Account',
+            target: 'https://www.google.com/',
+        },
+        {
+            icon: 'settings',
+            label: 'Settings',
+            target: 'https://www.google.com/',
+        },
+        {
+            icon: 'exit_to_app',
+            label: 'Logout',
+            target: () => alert('Logout'),
+        },
+    ],
+});
+
+export const withAvatar = s(Template, {
+    label: 'John Doe',
+    description: 'Admin',
+    size: 'xs',
+    avatar: true,
     items: [
         {
             icon: 'person',
