d0f2562a21e82bc2f9b6407b22bedda7af99c611
carl-mrchnd <66088573+carl-mrchnd@users.noreply.github.com>
2021-07-28 14:12:08 +0200

    feat: CU-aqwqey add CenteredLayoutTemplate (#188)

--- packages/react-admin-ui/src/templates/LoginTemplate.tsx
+++ packages/react-admin-ui/src/templates/LoginTemplate.tsx
@@ -1,26 +1,9 @@
 import { ReactNode } from 'react';
-import Image from '../atoms/Image';
-import Container from '../atoms/Container';
 import { WithImage, WithLogo } from '../withs';
+import CenteredLayoutTemplate from './layouts/CenteredLayoutTemplate';
 
-export function LoginTemplate({ image, logo, form }: LoginTemplateProps) {
-    return (
-        <Container
-            bgImage={image}
-            className={
-                'bg-cover h-screen py-10 px-3 xs:px-0 flex flex-col justify-center xs:justify-start items-center'
-            }
-        >
-            {logo && <Image expand={false} {...logo} className={'mx-auto max-w-xs xs:max-w-xxs'} />}
-            <div
-                className={
-                    'mt-4 p-10 xs:p-5 w-full max-w-md xs:max-w-full bg-clear rounded-lg xs:rounded-none shadow-block'
-                }
-            >
-                {form || ''}
-            </div>
-        </Container>
-    );
+export function LoginTemplate({ form, ...props }: LoginTemplateProps) {
+    return <CenteredLayoutTemplate {...props}>{form}</CenteredLayoutTemplate>;
 }
 
 export interface LoginTemplateProps extends WithImage, WithLogo {
