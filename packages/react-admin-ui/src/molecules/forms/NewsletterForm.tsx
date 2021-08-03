904e78defac964d58905e89e34d7a1f8e2669467
Olivier Hoareau <oha@greenberets.io>
2021-08-03 23:14:50 +0200

    Update NewsletterForm.tsx

--- packages/react-admin-ui/src/molecules/forms/NewsletterForm.tsx
+++ packages/react-admin-ui/src/molecules/forms/NewsletterForm.tsx
@@ -1,14 +1,3 @@
-/*
-import EmailField from "../../atoms/fields/EmailField";
-import Button from "../../atoms/Button";
-
-<form className={'flex xs:flex-col items-center mt-3'} onSubmit={handleSubmit(onSubmit)}>
-    <EmailField required register={register} variant={'outlined'} className={'min-w-full'} />
-    <Button color={color} variant={'contained'} className={'w-auto xs:w-full mt-4'}>
-        {btnLabel}
-    </Button>
-</form>
-*/
 import useForm from '../../hooks/useForm';
 import { BaseFormProps } from './BaseForm';
 import { EmailField } from '../../atoms';
@@ -17,9 +6,9 @@ export function NewsletterForm({ ...props }: NewsletterFormProps) {
     const { Form, SubmitButton, field } = useForm(props, 'newsletter');
 
     return (
-        <Form {...props}>
-            <EmailField {...field} />
-            <SubmitButton></SubmitButton>
+        <Form>
+            <EmailField {...field} required className={'min-w-full'} />
+            <SubmitButton className={'w-auto xs:w-full mt-4'} />
         </Form>
     );
 }
