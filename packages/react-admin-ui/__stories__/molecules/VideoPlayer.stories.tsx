57a104cdbabf614b60bc8c571da5377f494c32f7
cnote <56361858+mhcini@users.noreply.github.com>
2021-08-04 19:01:56 +0200

    feat(react-admin-ui): CU-rahdax VideoPlayer with btnLabel (#207)

--- packages/react-admin-ui/__stories__/molecules/VideoPlayer.stories.tsx
+++ packages/react-admin-ui/__stories__/molecules/VideoPlayer.stories.tsx
@@ -1,13 +1,14 @@
-import { s } from '../utils';
+import { args, a, s } from '../utils';
 import { VideoPlayer } from '../../src';
 
 export default {
     title: 'Molecules/VideoPlayer',
     component: VideoPlayer,
-    argTypes: {
+    argTypes: a({
         url: { control: 'text' },
         image: { control: 'object' },
-    },
+        btnLabel: args.btnLabel,
+    }),
 };
 
 const Template = (args) => <VideoPlayer {...args} />;
@@ -21,4 +22,10 @@ export const customIcon = s(Template, {
     url: 'https://www.youtube.com/embed/Z3xkHmC-KQE',
     image: { url: 'https://via.placeholder.com/419x219?text=Image', alt: 'an image' },
     icon: 'exit_to_app',
+});
+
+export const withButtonLabel = s(Template, {
+    url: 'https://www.youtube.com/embed/Z3xkHmC-KQE',
+    image: { url: 'https://via.placeholder.com/419x219?text=Image', alt: 'an image' },
+    btnLabel: 'Click Here',
 });
