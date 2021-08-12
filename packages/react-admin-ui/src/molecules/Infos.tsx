03940b5a49edb892001aa1ce9b403e14f59c3177
Havok <72014202+Lucas-COX@users.noreply.github.com>
2021-08-12 22:15:33 +0200

    feat: CU-1ck48bv create InfosItem (#223)

--- packages/react-admin-ui/src/molecules/Infos.tsx
+++ packages/react-admin-ui/src/molecules/Infos.tsx
@@ -3,6 +3,7 @@ import Text from '../atoms/Text';
 import useBlock from '../hooks/useBlock';
 import { AsComponent } from '../as';
 import { WithItemsOfInfos, WithColorOfText, WithTitle } from '../withs';
+import { InfosItem } from '../atoms/InfosItem';
 
 export function Infos({ items = [], title, ...props }: InfosProps) {
     const [bProps] = useBlock(props, { color: 'primary' });
@@ -10,10 +11,7 @@ export function Infos({ items = [], title, ...props }: InfosProps) {
         <Block {...bProps}>
             <Text text={title} variant={'title6'} />
             {items.map(({ label, value }, index) => (
-                <div key={index} className="flex justify-between mt-4 ">
-                    <Text text={label} className={'text-disabled'} />
-                    <Text text={value} />
-                </div>
+                <InfosItem label={label} value={value} key={index} />
             ))}
         </Block>
     );
