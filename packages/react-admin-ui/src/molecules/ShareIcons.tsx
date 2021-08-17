032649c227456a06dc8481ce44e97239636b291f
cnote <56361858+mhcini@users.noreply.github.com>
2021-08-17 09:26:29 +0200

    feat(react-admin-ui): CU-1ck48h0 ShareIcons > .map => Items (creer ShareIcon) (#240)

--- packages/react-admin-ui/src/molecules/ShareIcons.tsx
+++ packages/react-admin-ui/src/molecules/ShareIcons.tsx
@@ -1,24 +1,13 @@
 import clsx from 'clsx';
-import { Icon } from '../atoms/Icon';
-import boxClass from '../utils/boxClass';
-import shareIconTypeClass from '../mappings/share-icon-types';
 import { WithItemsOfShareIcons } from '../withs';
 import { AsBox } from '../as';
+import Items from '../atoms/Items';
+import ShareIcon from './ShareIcon';
 
-export function ShareIcons({ className, items = [], color, variant }: ShareIconsProps) {
+export function ShareIcons({ className, items = [], ...props }: ShareIconsProps) {
     return (
         <div className={clsx('flex flex-row space-x-5', className)}>
-            {items.map((item, index) => (
-                <div
-                    key={index}
-                    className={clsx(
-                        boxClass({ color: color, variant: variant }),
-                        'flex rounded-full h-16 w-16 items-center justify-center',
-                    )}
-                >
-                    <Icon icon={shareIconTypeClass(item.type)} size={'xl'} color={color} onClick={item.target} />
-                </div>
-            ))}
+            <Items items={items} component={ShareIcon} {...props} />
         </div>
     );
 }
