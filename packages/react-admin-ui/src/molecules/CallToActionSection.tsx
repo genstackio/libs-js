ae18c6ee7054128f0fccab8858e61dd11c6e08ff
Olivier Hoareau <oha@greenberets.io>
2021-08-17 19:37:40 +0200

    Update CallToActionSection.tsx

--- packages/react-admin-ui/src/molecules/CallToActionSection.tsx
+++ packages/react-admin-ui/src/molecules/CallToActionSection.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx';
 import Buttons from '../atoms/Buttons';
 import HeadingText from '../atoms/HeadingText';
 import Block from '../atoms/Block';
@@ -11,26 +10,15 @@ import useHeadingText from '../hooks/useHeadingText';
 export function CallToActionSection({ className, ...props }: CallToActionSectionProps) {
     const [bProps, rest3] = useBlock(props);
     const [htProps, rest2] = useHeadingText(rest3);
-    const [buttonsProps] = useButtons(rest2);
+    const [btProps] = useButtons(rest2);
     return (
         <Block
             {...bProps}
             contentClassName={'flex py-12 px-4 sm:px-6 lg:py-16 lg:px-8 justify-between items-center xs:flex-col'}
         >
-            <HeadingText
-                {...htProps}
-                title={'Ready to dive?'}
-                subtitle={'Start your free trial today.'}
-                titleClassName={'mb-2'}
-                variant={'ctasection'}
-            />
-            <div className={clsx(className)}>
-                <Buttons
-                    className={clsx('mb-2 px-5 py-3')}
-                    btnLabel={'Get Started'}
-                    btn2Label={'Learn more'}
-                    {...buttonsProps}
-                />
+            <HeadingText {...htProps} titleClassName={'mb-2'} variant={'ctasection'} />
+            <div className={className}>
+                <Buttons className={'mb-2 px-5 py-3'} btnLabel={'Get Started'} btn2Label={'Learn more'} {...btProps} />
             </div>
         </Block>
     );
