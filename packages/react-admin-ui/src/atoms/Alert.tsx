50f1c272fbb9d80f28d22daafb6dd473746f015b
Lucas-COX <lucas.cox@epitech.eu>
2021-08-20 15:07:07 +0200

    feat: CU-1cynkkk add button and icon props to Alert

--- packages/react-admin-ui/src/atoms/Alert.tsx
+++ packages/react-admin-ui/src/atoms/Alert.tsx
@@ -1,15 +1,51 @@
+import clsx from 'clsx';
 import CloseIcon from '@material-ui/icons/Close';
 import Div from './Div';
 import Clickable from './Clickable';
 import Panel, { PanelProps } from './Panel';
 import { useToggle } from '../hooks/useToggle';
-import { WithClosable } from '../withs';
+import {WithButton, WithCenter, WithClosable, WithIcon} from '../withs';
+import Button from './Button';
+import { box_variant } from '../mappings/box-variants';
+import Icon from "./Icon";
 
-export function Alert({ children, onClose, closable = false, ...props }: AlertProps) {
+export function Alert({
+    children,
+    onClose,
+    closable = false,
+    btnClassName,
+    btnColor,
+    btnCorner,
+    btnIcon,
+    btnEndIcon,
+    btnType,
+    btnTarget,
+    btnLabel,
+    btnSize,
+    color,
+    icon,
+    iconSize,
+    center,
+    ...props
+}: AlertProps) {
     const [show, handleClick] = useToggle(true, onClose);
     return show ? (
-        <Panel {...props}>
+        <Panel {...props} color={color} className={clsx('space-x-8', center && 'items-center justify-center')}>
+            <Icon icon={icon} size={iconSize} />
             <Div full>{children}</Div>
+            {btnLabel && (
+                <Button
+                    variant={btnType as box_variant}
+                    color={btnColor || color}
+                    corner={btnCorner}
+                    icon={btnIcon}
+                    endIcon={btnEndIcon}
+                    size={btnSize}
+                    className={btnClassName}
+                >
+                    {btnLabel}
+                </Button>
+            )}
             <Clickable visible={closable} onClick={handleClick}>
                 <CloseIcon />
             </Clickable>
@@ -17,7 +53,7 @@ export function Alert({ children, onClose, closable = false, ...props }: AlertPr
     ) : null;
 }
 
-export interface AlertProps extends PanelProps, WithClosable {}
+export interface AlertProps extends PanelProps, WithClosable, WithButton, WithIcon, WithCenter {}
 
 // noinspection JSUnusedGlobalSymbols
 export default Alert;
