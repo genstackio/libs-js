814a87b3da5f7b5a07f2cb42a5b01f8f76e08165
Olivier Hoareau <oha@greenberets.io>
2022-04-21 18:09:56 +0200

    fix(react-contexts): fix

--- packages/react-contexts/src/hooks/useLazyQueryApi.ts
+++ packages/react-contexts/src/hooks/useLazyQueryApi.ts
@@ -4,7 +4,10 @@ const defaultOptions = {};
 
 export const useLazyQueryApi = (name: string, options: any = defaultOptions) => {
     const { getQuery, useLazyQuery } = useApi();
-    return useLazyQuery(getQuery(name, options), options);
+    return useLazyQuery(
+        getQuery(name, options),
+        options ? {...(options || {}), ...(options!.query ? {query: getQuery(options!.query!, options)} : {})} : undefined
+    );
 };
 
 export default useLazyQueryApi;
