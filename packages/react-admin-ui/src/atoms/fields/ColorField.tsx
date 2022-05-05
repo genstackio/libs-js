5bad6975aae5209396e1c59610167a6d24f869e0
carl-mrchnd <66088573+carl-mrchnd@users.noreply.github.com>
2022-05-05 18:44:13 +0200

    fix(react-admin-ui): padding in color field input (#256)
    * fix(react-admin-ui): padding in color field input
    
    * Update ColorField.tsx
    
    Co-authored-by: Olivier Hoareau <oha@greenberets.io>

--- packages/react-admin-ui/src/atoms/fields/ColorField.tsx
+++ packages/react-admin-ui/src/atoms/fields/ColorField.tsx
@@ -1,8 +1,18 @@
 import clsx from 'clsx';
 import TextField, { TextFieldProps } from './TextField';
 
+const classes = {input: 'pt-0 pb-0 pr-0 pl-0'};
+
 export function ColorField({ className, ...props }: ColorFieldProps) {
-    return <TextField kind={'color'} type={'color'} {...props} className={clsx('x-field-min-h', className)} />;
+    return (
+        <TextField
+            kind={'color'}
+            type={'color'}
+            {...props}
+            className={clsx('x-field-min-h', className)}
+            classes={classes}
+        />
+    );
 }
 
 export type ColorFieldProps = TextFieldProps;
