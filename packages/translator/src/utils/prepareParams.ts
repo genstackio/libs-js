ca7eb65a704be0cc3016481828e048cd76adf416
Olivier Hoareau <oha@greenberets.io>
2023-01-02 15:09:54 +0100

    fix(translator): fix

--- packages/translator/src/utils/prepareParams.ts
+++ packages/translator/src/utils/prepareParams.ts
@@ -1,11 +1,11 @@
 export function prepareParams(a: string) {
     return a
-        .replace(/(\{\{([a-z0-9_]+)}})/gi, (match, p1, p2) => `<X>${p2}</X>`)
         .replace(/>/gi, () => `<X1>_</X1>`)
         .replace(/</gi, () => `<X2>_</X2>`)
         .replace(/&/gi, () => `<X3>_</X3>`)
         .replace(/©/gi, () => `<X4>_</X4>`)
         .replace(/®/gi, () => `<X5>_</X5>`)
+        .replace(/(\{\{([a-z0-9_]+)}})/gi, (match, p1, p2) => `<X>${p2}</X>`)
     ;
 }
 
