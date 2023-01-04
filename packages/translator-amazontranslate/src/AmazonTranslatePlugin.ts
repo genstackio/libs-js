400037a8fb479c6f66ac1044cca7ae782741108d
Olivier Hoareau <oha@greenberets.io>
2023-01-04 01:22:36 +0100

    fix(translate-amazontranslate): fix

--- packages/translator-amazontranslate/src/AmazonTranslatePlugin.ts
+++ packages/translator-amazontranslate/src/AmazonTranslatePlugin.ts
@@ -18,28 +18,39 @@ export class AmazonTranslatePlugin extends AbstractTranslatorPlugin<AWS.Translat
         const items = await t!.listLanguages({}).promise();
         return items.Languages?.reduce((acc, l) => Object.assign(acc, {[l.LanguageCode]: true}), {} as Record<string, true>) || {} as Record<string, true>;
     }
+    protected async wait(n: number) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(true);
+            }, n);
+        })
+    }
     async translate(t: AWS.Translate, texts: string[], sourceLocale: any, targetLocale: any, options: Record<string, any> | undefined): Promise<any[]> {
         let oldTexts = [...texts];
         let newTexts: {TranslatedText}[] = [];
 
         let joinedText: string = '';
 
+        let ii: number = 0;
+
         while(oldTexts.length) {
             joinedText = '';
             let i = 0;
-            while(oldTexts.length && ((joinedText.length + oldTexts[0].length) < 9900)) {
+            while(oldTexts.length && ((joinedText.length + oldTexts[0].length) < 9700)) { // max 9700 (strictly below 10000) to avoid max size error
                 joinedText = `${joinedText || ''}${joinedText ? '<div translate=no>LINE</div>' : ''}${oldTexts.shift()}`;
                 i++;
             }
             try {
+                if (ii % 5) await this.wait(1000); // wait 1s to avoid rate exceeded error
                 const x = await this.translateText(t, joinedText, sourceLocale, targetLocale, options);
                 newTexts = [...newTexts, ...(x.TranslatedText || '').split(/<div translate=no>LINE<\/div>/).map(xx => ({TranslatedText: xx}))];
             } catch (e: any) {
-                console.error(`ERROR [amazontranslate] ${e.message}`);
+                console.error(`ERROR [amazontranslate] ${e.message} for original text:\n\n${joinedText}`);
                 for (let j = 0; j < i; j++) {
                     newTexts.push({TranslatedText: ''});
                 }
             }
+            ii++;
         }
 
         return newTexts;
