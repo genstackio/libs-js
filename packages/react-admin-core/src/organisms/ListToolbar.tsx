31bdb66ef609264ec265649d5a307da65d9d1532
Olivier Hoareau <oha@greenberets.io>
2023-01-23 14:18:31 +0100

    fix(react-admin-core): fix

--- packages/react-admin-core/src/organisms/ListToolbar.tsx
+++ packages/react-admin-core/src/organisms/ListToolbar.tsx
@@ -25,57 +25,55 @@ export function ListToolbar({
     const filterNames = Object.keys(filters || {});
     const hasFilters = !!filterNames.length;
 
-    return (
-        (hasFilters || !!onNewClick || displayPage) && (
-            <div className={'w-full flex flex-col'}>
-                {hasFilters && (
-                    <div className={'flex'}>
-                        {filterNames.map((f) => (
-                            <div key={f} className={'flex-1 flex flex-col'}>
-                                <MenuItem
-                                    active={f === filterName}
-                                    label={t([filters[f]?.label, f])}
-                                    target={`/bla/${f}`}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                )}
-                <div className={''}>
-                    <div className={clsx('flex space-x-4 items-center', !searchSwitch ? 'mb-5' : 'mb-1')}>
-                        {!!onNewClick && (
-                            <Button
-                                onClick={
-                                    onNewClick
-                                        ? onNewClick
-                                        : (list['globalActions'] || []).includes('add')
-                                        ? goNew
-                                        : undefined
-                                }
-                                icon={'fa-fas--plus'}
-                                variant={'contained'}
-                            >
-                                {t('button_add_label')}
-                            </Button>
-                        )}
-                        {(!!items.length || !loading) && (page.index > 0 || !!nextCursor) && (
-                            <Tag variant={'contained'} className={'rounded'} text={`Page ${page.index + 1}`} />
-                        )}
-                        {!!searchSwitch && (
-                            <div className={'flex flex-col justify-center -mt-2'}>
-                                <SwitchField
-                                    name={'searchMode'}
-                                    defaultValues={switchDefaultValues}
-                                    label={'opensearch'}
-                                    onChange={handleSwitchChange}
-                                />
-                            </div>
-                        )}
-                    </div>
+    return hasFilters || !!onNewClick || displayPage ? (
+        <div className={'w-full flex flex-col'}>
+            {hasFilters && (
+                <div className={'flex'}>
+                    {filterNames.map((f) => (
+                        <div key={f} className={'flex-1 flex flex-col'}>
+                            <MenuItem
+                                active={f === filterName}
+                                label={t([filters[f]?.label, f])}
+                                target={`/bla/${f}`}
+                            />
+                        </div>
+                    ))}
+                </div>
+            )}
+            <div className={''}>
+                <div className={clsx('flex space-x-4 items-center', !searchSwitch ? 'mb-5' : 'mb-1')}>
+                    {!!onNewClick && (
+                        <Button
+                            onClick={
+                                onNewClick
+                                    ? onNewClick
+                                    : (list['globalActions'] || []).includes('add')
+                                    ? goNew
+                                    : undefined
+                            }
+                            icon={'fa-fas--plus'}
+                            variant={'contained'}
+                        >
+                            {t('button_add_label')}
+                        </Button>
+                    )}
+                    {(!!items.length || !loading) && (page.index > 0 || !!nextCursor) && (
+                        <Tag variant={'contained'} className={'rounded'} text={`Page ${page.index + 1}`} />
+                    )}
+                    {!!searchSwitch && (
+                        <div className={'flex flex-col justify-center -mt-2'}>
+                            <SwitchField
+                                name={'searchMode'}
+                                defaultValues={switchDefaultValues}
+                                label={'opensearch'}
+                                onChange={handleSwitchChange}
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
-        )
-    );
+        </div>
+    ) : null;
 }
 
 export interface ListToolbarProps {
