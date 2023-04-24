50e67e2722ed710522f3866ef55648d842af86c1
Olivier Hoareau <oha@greenberets.io>
2023-04-24 20:10:57 +0200

    fix(*): fix

--- packages/react-admin-ui/src/atoms/fields/QuerySelectField.tsx
+++ packages/react-admin-ui/src/atoms/fields/QuerySelectField.tsx
@@ -11,27 +11,31 @@ function useQuerySelectParams({
     query,
     dataKey,
     convert,
+    options,
     kind = 'object',
 }: {
     query?: string;
     dataKey?: string;
     convert?: Function;
     kind?: string;
-}): { query: string; dataKey: string; convert: Function } {
+    options?: Function;
+}): { query: string; dataKey: string; convert: Function; options: Function } {
     query = useMemo(() => query || `FIND_${kind.toUpperCase()}S`, [query, kind]);
     dataKey = useMemo(() => dataKey || `find${kind.slice(0, 1).toUpperCase()}${kind.slice(1)}s`, [dataKey, kind]);
     convert = useMemo(() => convert || defaultConvert, [convert]);
+    options = useMemo(() => options || undefined, [options]);
 
-    return useMemo(() => ({ query, dataKey, convert }), [query, dataKey, convert]) as {
+    return useMemo(() => ({ query, dataKey, convert, options }), [query, dataKey, convert, options]) as {
         query: string;
         dataKey: string;
         convert: Function;
+        options: Function;
     };
 }
 
-export function QuerySelectField({ query, dataKey, convert, ...props }: QuerySelectFieldProps) {
-    const p = useQuerySelectParams({ query, dataKey, convert, ...props });
-    const { data, loading } = useQueryApi(p.query);
+export function QuerySelectField({ query, dataKey, convert, options, ...props }: QuerySelectFieldProps) {
+    const p = useQuerySelectParams({ query, dataKey, convert, options, ...props });
+    const { data, loading } = useQueryApi(p.query, p.options ? p.options(props) : undefined);
     const items = (((data || {})[p.dataKey] || {}).items || []).map(p.convert);
 
     return (
@@ -45,12 +49,14 @@ export function QuerySelectField({ query, dataKey, convert, ...props }: QuerySel
 export interface BaseQuerySelectFieldProps extends SelectFieldProps {
     query?: string;
     convert?: Function;
+    options?: Function;
     dataKey?: string;
 }
 
 export interface QuerySelectFieldProps extends BaseQuerySelectFieldProps {
     query?: string;
     convert?: Function;
+    options?: Function;
     dataKey?: string;
 }
 
