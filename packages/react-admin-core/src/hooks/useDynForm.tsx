753bb97b485250e3d01a232dff9971cbbb3cfc0d
Olivier Hoareau <oha@greenberets.io>
2023-04-25 12:27:11 +0200

    fix(react-admin-core): fix

--- packages/react-admin-core/src/hooks/useDynForm.tsx
+++ packages/react-admin-core/src/hooks/useDynForm.tsx
@@ -5,6 +5,7 @@ import applyContext from "../utils/applyContext";
 import Contents from "@genstackio/react-admin-ui/lib/molecules/Contents";
 
 const defaultContext: any = {};
+const defaultContent: any[] = [];
 
 export function useDynForm(definition: any, view: string, props: any, context: any = defaultContext) {
     const value = useMemo(
@@ -20,31 +21,36 @@ export function useDynForm(definition: any, view: string, props: any, context: a
 
     const options = useMemo(() => ({ field, SubmitButton }), [field, SubmitButton]);
 
-    const { content = [] } = useMemo(() => applyContext((value.definition || {})[view] || {}, context), [view, value, context]) || {};
+    const { content = defaultContent } = useMemo(() => applyContext((value.definition || {})[view] || {}, context), [view, value, context]) || {};
 
-    const DynForm = ({children}: any) => (
+    const DynForm = useMemo(() => ({children}: any) => (
         <DynamicFormContextProvider value={value}>
-            <Form customChildren={children} className={'w-full'} submitting={form.rhf.formState.isSubmitting}>
+            <Form customChildren={children} className={'w-full'}>
                 <Contents content={content} options={options} />
             </Form>
         </DynamicFormContextProvider>
-    );
+    ), [content, options]);
 
     const state = useMemo(() => ({
-        ...form.rhf.formState,
-    }), [form.rhf.formState]);
+        isSubmitting: form.rhf.formState.isSubmitting,
+        isSubmitted: form.rhf.formState.isSubmitted,
+        isDirty: form.rhf.formState.isDirty,
+        isValid: form.rhf.formState.isValid,
+        isValidating: form.rhf.formState.isValidating,
+    }), [
+        form.rhf.formState.isSubmitting,
+        form.rhf.formState.isSubmitted,
+        form.rhf.formState.isDirty,
+        form.rhf.formState.isValid,
+        form.rhf.formState.isValidating,
+    ]);
 
     return useMemo(() => [form.rhf.handleSubmit, DynForm, state, form.rhf], [form.rhf.handleSubmit, DynForm, state, form.rhf]) as [Function, ComponentType<any>, {
         isDirty: boolean;
-        dirtyFields: any;
         isSubmitted: boolean;
-        isSubmitSuccessful: boolean;
-        submitCount: number;
-        touchedFields: any;
         isSubmitting: boolean;
         isValidating: boolean;
         isValid: boolean;
-        errors: any;
     }, any];
 }
 
