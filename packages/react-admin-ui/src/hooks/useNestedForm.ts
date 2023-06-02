96e5cd68f8e4751a4b2d1c3c29dfd98113657b58
Olivier Hoareau <oha@greenberets.io>
2023-06-02 18:00:13 +0200

    fix(react-admin-ui): fix

---
+++ packages/react-admin-ui/src/hooks/useNestedForm.ts
@@ -0,0 +1,16 @@
+import {useCallback, useMemo} from "react";
+import useForm from "./useForm";
+
+const formOptions = {nested: true};
+export function useNestedForm({value, onSubmit}: any, marshall?: Function, unmarshall?: Function ) {
+    const preparedValue = useMemo(() => marshall ? marshall(value) : value, [value, marshall]);
+    const preparedOnSubmit = useCallback((data: any) => {
+        onSubmit?.(unmarshall ? unmarshall(data) : data);
+    }, [onSubmit, unmarshall]);
+    const formProps = useMemo(() => ({defaultValues: preparedValue, onSubmit: preparedOnSubmit}), [preparedValue, preparedOnSubmit]);
+
+    return useForm(formProps, undefined, formOptions);
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default useNestedForm;
