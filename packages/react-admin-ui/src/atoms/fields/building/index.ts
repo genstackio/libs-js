3d2953b4f610ef5a80248364a72b48fe10beb360
emilienbuguet <72021370+emilienbuguet@users.noreply.github.com>
2023-06-02 18:01:33 +0200

    fix(fields): export BuildingThicknessField (#272)

--- packages/react-admin-ui/src/atoms/fields/building/index.ts
+++ packages/react-admin-ui/src/atoms/fields/building/index.ts
@@ -4,3 +4,4 @@ export * from './BuildingPressureField';
 export * from './BuildingSizeField';
 export * from './BuildingVolumeField';
 export * from './BuildingYoungModuleField';
+export * from './BuildingThicknessField';
